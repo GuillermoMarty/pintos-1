6ca75cf22008f0a1b82f325b0adcac8263cf1e3b
Doug McLaughlin <dmclaughlin@homeadvisor.com>
2017-03-12 13:21:07 -0600

    Changed sema_down cmp_pri to cmp_elem

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -71,7 +71,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_insert_ordered(&sema-waiters, &thread_current()->elem, (list_less_func *) &cmp_priority, NULL);
+      list_insert_ordered(&sema-waiters, &thread_current()->elem, (list_less_func *) &cmp_elem, NULL);
       thread_block ();
     }
   sema->value--;
